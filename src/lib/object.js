9bbce016c13e27aaf4f67758c72ca44c3fa54201
Kohei Yoshino <kohei.yoshino@gmail.com>
2024-05-15 19:01:03 -0400

    Add deproxify method

--- src/lib/object.js
+++ src/lib/object.js
@@ -13,3 +13,11 @@ export const isObject = (input) =>
  */
 export const isObjectArray = (input) =>
   Array.isArray(input) && /** @type {any[]} */ (input).every((item) => isObject(item));
+
+/**
+ * Convert a Proxy to the original object. The built-in `structuredClone()` method throws with a
+ * Proxy, so this can be used instead. The name of `toRaw` is derived from the equivalent in Vue.
+ * @param {object} obj - Proxified object.
+ * @returns {object} Deproxified object.
+ */
+export const toRaw = (obj) => JSON.parse(JSON.stringify(obj));
