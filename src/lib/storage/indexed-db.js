0429c46ac937889eacdae63b4ec5b827792d1abf
Kohei Yoshino <kohei.yoshino@gmail.com>
2024-05-16 19:49:59 -0400

    Enhance IndexedDB wrapper with index and search methods

--- src/lib/storage/indexed-db.js
+++ src/lib/storage/indexed-db.js
@@ -1,6 +1,15 @@
 /* eslint-disable jsdoc/require-jsdoc */
 
 /**
+ * Arguments to be passed to `IDBObjectStore.createIndex()`.
+ * @typedef {object} DatabaseIndex
+ * @property {string} name Index name.
+ * @property {string | string[]} keyPath Key path(s).
+ * @property {IDBIndexParameters} [options] Index options.
+ * @see https://developer.mozilla.org/en-US/docs/Web/API/IDBObjectStore/createIndex#parameters
+ */
+
+/**
  * Implement a wrapper for the IndexedDB API, making it easier to use the client-side database with
  * auto-upgrades and convenient Promise methods.
  */
@@ -24,14 +33,35 @@ export default class IndexedDB {
   #storeName = '';
 
   /**
+   * Store options to be passed to `IDBDatabase.createObjectStore()`. Note that this cannot be
+   * altered after the store is created.
+   * @type {IDBObjectStoreParameters}
+   */
+  #storeOptions = {};
+
+  /**
+   * Index options to be passed to `IDBObjectStore.createIndex()`.
+   * @type {DatabaseIndex[]}
+   */
+  #indexes = [];
+
+  /**
    * Initialize a new `IndexedDB` instance.
    * @param {string} databaseName - Database name.
    * @param {string} storeName - Store name.
+   * @param {object} [options] - Options.
+   * @param {string | string[]} [options.keyPath] - Option for `createObjectStore()`.
+   * @param {boolean} [options.autoIncrement] - Option for `createObjectStore()`.
+   * @param {DatabaseIndex[]} [options.indexes] - Arguments for `createIndex()`.
+   * @see https://developer.mozilla.org/en-US/docs/Web/API/IDBDatabase/createObjectStore
+   * @see https://stackoverflow.com/questions/33852508/how-to-create-an-indexeddb-composite-key
    */
-  constructor(databaseName, storeName) {
+  constructor(databaseName, storeName, { keyPath, autoIncrement, indexes = [] } = {}) {
     this.#database = undefined;
     this.#databaseName = databaseName;
     this.#storeName = storeName;
+    this.#storeOptions = { keyPath, autoIncrement };
+    this.#indexes = indexes;
   }
 
   /**
@@ -44,7 +74,18 @@ export default class IndexedDB {
       const request = globalThis.indexedDB.open(this.#databaseName, version);
 
       request.onupgradeneeded = () => {
-        request.result.createObjectStore(this.#storeName);
+        const database = request.result;
+        const storeName = this.#storeName;
+
+        const store = database.objectStoreNames.contains(storeName)
+          ? database.transaction(storeName, 'readwrite').objectStore(storeName)
+          : database.createObjectStore(storeName, this.#storeOptions);
+
+        this.#indexes.forEach(({ name, keyPath, options }) => {
+          if (!store.indexNames.contains(name)) {
+            store.createIndex(name, keyPath, options);
+          }
+        });
       };
 
       request.onsuccess = () => {
@@ -58,10 +99,20 @@ export default class IndexedDB {
    * @returns {Promise<IDBDatabase>} Database.
    */
   async #getDatabase() {
+    let upgradeNeeded = false;
     let database = await this.#openDatabase();
     const { version, objectStoreNames } = database;
+    const storeName = this.#storeName;
 
-    if (![...objectStoreNames].includes(this.#storeName)) {
+    if (objectStoreNames.contains(storeName)) {
+      const store = database.transaction(storeName).objectStore(storeName);
+
+      upgradeNeeded = this.#indexes.some(({ name }) => !store.indexNames.contains(name));
+    } else {
+      upgradeNeeded = true;
+    }
+
+    if (upgradeNeeded) {
       database.close();
       database = await this.#openDatabase(version + 1);
     }
@@ -83,13 +134,10 @@ export default class IndexedDB {
   async #query(getRequest) {
     this.#database ??= await this.#getDatabase();
 
-    const transaction = /** @type {IDBDatabase} */ (this.#database).transaction(
-      [this.#storeName],
-      'readwrite',
-    );
-
-    const store = transaction.objectStore(this.#storeName);
-    const request = getRequest(store);
+    const database = /** @type {IDBDatabase} */ (this.#database);
+    const storeName = this.#storeName;
+    const transaction = database.transaction(storeName, 'readwrite');
+    const request = getRequest(transaction.objectStore(storeName));
 
     if (request) {
       return new Promise((resolve) => {
@@ -107,7 +155,7 @@ export default class IndexedDB {
   }
 
   /**
-   * Save a single entry.
+   * Save a value with an out-of-line key.
    * @param {any} key - Key.
    * @param {any} value - Value.
    * @returns {Promise<any>} Key.
@@ -117,13 +165,22 @@ export default class IndexedDB {
   }
 
   /**
-   * Save multiple entries.
-   * @param {[any, any][]} entries - Key/value pairs.
+   * Save a value with an inline key.
+   * @param {any} value - Value.
    * @returns {Promise<any>} Key.
    */
-  async saveEntries(entries) {
+  async put(value) {
+    return this.#query((store) => store.put(value));
+  }
+
+  /**
+   * Save multiple records.
+   * @param {[any, any][]} records - Key/value pairs.
+   * @returns {Promise<any>} Key.
+   */
+  async saveEntries(records) {
     return this.#query((store) => {
-      entries.forEach(([key, value]) => {
+      records.forEach(([key, value]) => {
         store.put(value, key);
       });
     });
@@ -139,7 +196,7 @@ export default class IndexedDB {
   }
 
   /**
-   * Retrieve all keys.
+   * Retrieve all the keys.
    * @returns {Promise<any[]>} Keys.
    */
   async keys() {
@@ -147,7 +204,7 @@ export default class IndexedDB {
   }
 
   /**
-   * Retrieve all values.
+   * Retrieve all the values.
    * @returns {Promise<any[]>} Values.
    */
   async values() {
@@ -155,7 +212,7 @@ export default class IndexedDB {
   }
 
   /**
-   * Retrieve all entries.
+   * Retrieve all the records.
    * @returns {Promise<[any, any][]>} Key/value pairs.
    */
   async entries() {
@@ -165,21 +222,104 @@ export default class IndexedDB {
   }
 
   /**
-   * Delete an entry by key.
+   * Find one or more records using a cursor.
+   * @param {object} args - Arguments.
+   * @param {(record: any) => boolean} [args.callback] - A function to execute for each record.
+   * @param {string} [args.index] - Index name.
+   * @param {IDBValidKey | IDBKeyRange} [args.query] - Query option for `openCursor()`.
+   * @param {IDBCursorDirection} [args.direction] - The direction option for `openCursor()`.
+   * @param {boolean} [args.multiple] - Whether to return all matching records.
+   * @returns {Promise<any | any[]>} Found record(s).
+   */
+  async #search({
+    callback = undefined,
+    index = undefined,
+    query = undefined,
+    direction = 'next',
+    multiple = false,
+  }) {
+    return new Promise((resolve) => {
+      this.#query((store) => {
+        const request = (index ? store.index(index) : store).openCursor(query, direction);
+        /** @type {any[]} */
+        const records = [];
+
+        request.onsuccess = () => {
+          const cursor = request.result;
+
+          if (cursor) {
+            const { value } = cursor;
+
+            if (typeof callback === 'function' ? callback(value) : true) {
+              if (multiple) {
+                records.push(value);
+                cursor.continue();
+              } else {
+                resolve(value);
+              }
+            } else {
+              cursor.continue();
+            }
+          } else {
+            resolve(multiple ? records : undefined);
+          }
+        };
+      });
+    });
+  }
+
+  /**
+   * Find the first record that matches the condition.
+   * @param {(record: any) => boolean} [callback] - A function to execute for each record.
+   * @param {object} [options] - Options.
+   * @param {string} [options.index] - Index.
+   * @param {IDBValidKey | IDBKeyRange} [options.query] - Query option for `openCursor()`.
+   * @returns {Promise<any>} Found record.
+   */
+  async find(callback, { index, query } = {}) {
+    return this.#search({ callback, index, query });
+  }
+
+  /**
+   * Find the last record that matches the condition.
+   * @param {(record: any) => boolean} [callback] - A function to execute for each record.
+   * @param {object} [options] - Options.
+   * @param {string} [options.index] - Index.
+   * @param {IDBValidKey | IDBKeyRange} [options.query] - Query option for `openCursor()`.
+   * @returns {Promise<any>} Found record.
+   */
+  async findLast(callback, { index, query } = {}) {
+    return this.#search({ callback, index, query, direction: 'prev' });
+  }
+
+  /**
+   * Find all the records that match the condition.
+   * @param {(record: any) => boolean} [callback] - A function to execute for each record.
+   * @param {object} [options] - Options.
+   * @param {string} [options.index] - Index.
+   * @param {IDBValidKey | IDBKeyRange} [options.query] - Query option for `openCursor()`.
+   * @returns {Promise<any[]>} Found records.
+   */
+  async filter(callback, { index, query } = {}) {
+    return this.#search({ callback, index, query, multiple: true });
+  }
+
+  /**
+   * Delete an record by key.
    * @param {any} key - Key.
    * @returns {Promise<void>} Result.
    */
   async delete(key) {
-    return this.#query((store) => store.delete(key));
+    await this.#query((store) => store.delete(key));
   }
 
   /**
-   * Delete multiple entries by keys.
+   * Delete multiple records by keys.
    * @param {any[]} keys - Property keys.
    * @returns {Promise<void>} Result.
    */
   async deleteEntries(keys) {
-    return this.#query((store) => {
+    await this.#query((store) => {
       keys.forEach((key) => {
         store.delete(key);
       });
@@ -187,10 +327,10 @@ export default class IndexedDB {
   }
 
   /**
-   * Delete all entries.
+   * Delete all the records.
    * @returns {Promise<void>} Result.
    */
   async clear() {
-    return this.#query((store) => store.clear());
+    await this.#query((store) => store.clear());
   }
 }
---
+++ src/lib/storage/indexed-db.spec.js
@@ -0,0 +1,149 @@
+/* eslint-disable jsdoc/require-jsdoc */
+
+import { IDBKeyRange, indexedDB } from 'fake-indexeddb';
+import { describe, expect, test } from 'vitest';
+import IndexedDB from '$lib/storage/indexed-db';
+
+// https://github.com/vitest-dev/vitest/discussions/908
+globalThis.indexedDB = indexedDB;
+
+describe('Test IndexedDB', () => {
+  test('Without options', async () => {
+    const db = new IndexedDB('vite', 'test-1');
+
+    expect(await db.keys()).toEqual([]);
+    expect(await db.set('one', 1)).toEqual('one');
+    expect(await db.set('two', '2')).toEqual('two');
+    expect(await db.set('three', true)).toEqual('three');
+    expect(await db.set('four', [1, 2, 3])).toEqual('four');
+    expect(await db.set('five', { a: 1, b: 2 })).toEqual('five');
+
+    expect(await db.get('none')).toEqual(undefined);
+    expect(await db.get('one')).toEqual(1);
+    expect(await db.get('two')).toEqual('2');
+    expect(await db.get('three')).toEqual(true);
+    expect(await db.get('four')).toEqual([1, 2, 3]);
+    expect(await db.get('five')).toEqual({ a: 1, b: 2 });
+
+    expect(await db.keys()).toEqual(['five', 'four', 'one', 'three', 'two']);
+    expect(await db.values()).toEqual([{ a: 1, b: 2 }, [1, 2, 3], 1, true, '2']);
+    expect(await db.entries()).toEqual([
+      ['five', { a: 1, b: 2 }],
+      ['four', [1, 2, 3]],
+      ['one', 1],
+      ['three', true],
+      ['two', '2'],
+    ]);
+
+    expect(await db.delete('one')).toEqual(undefined);
+    expect(await db.keys()).toEqual(['five', 'four', 'three', 'two']);
+    expect(await db.deleteEntries(['two', 'three'])).toEqual(undefined);
+    expect(await db.keys()).toEqual(['five', 'four']);
+    expect(await db.clear()).toEqual(undefined);
+    expect(await db.keys()).toEqual([]);
+  });
+
+  test('Auto generating keys', async () => {
+    const db = new IndexedDB('vite', 'test-2', { keyPath: 'id', autoIncrement: true });
+
+    expect(await db.put({ title: 'one' })).toEqual(1);
+    expect(await db.put({ title: 'two' })).toEqual(2);
+    expect(await db.put({ title: 'three' })).toEqual(3);
+
+    expect(await db.get(1)).toEqual({ id: 1, title: 'one' });
+    expect(await db.get(2)).toEqual({ id: 2, title: 'two' });
+    expect(await db.get(3)).toEqual({ id: 3, title: 'three' });
+
+    expect(await db.keys()).toEqual([1, 2, 3]);
+    expect(await db.delete(2)).toEqual(undefined);
+    expect(await db.keys()).toEqual([1, 3]);
+  });
+
+  test('Composite key', async () => {
+    const db = new IndexedDB('vite', 'test-3', { keyPath: ['userId', 'productId'] });
+
+    expect(await db.put({ userId: 123, productId: 456 })).toEqual([123, 456]);
+    expect(await db.put({ userId: 234, productId: 567 })).toEqual([234, 567]);
+
+    expect(await db.get([123, 456])).toEqual({ userId: 123, productId: 456 });
+    expect(await db.get([234, 567])).toEqual({ userId: 234, productId: 567 });
+
+    expect(await db.keys()).toEqual([
+      [123, 456],
+      [234, 567],
+    ]);
+    expect(await db.delete([123, 456])).toEqual(undefined);
+    expect(await db.keys()).toEqual([[234, 567]]);
+  });
+
+  test('Find items', async () => {
+    const db = new IndexedDB('vite', 'test-4', {
+      keyPath: 'id',
+      autoIncrement: true,
+      indexes: [{ name: 'year', keyPath: 'year' }],
+    });
+
+    expect(await db.put({ year: 2023, category: 'book' })).toEqual(1);
+    expect(await db.put({ year: 2024, category: 'book' })).toEqual(2);
+    expect(await db.put({ year: 2025, category: 'book' })).toEqual(3);
+    expect(await db.put({ year: 2023, category: 'software' })).toEqual(4);
+    expect(await db.put({ year: 2024, category: 'software' })).toEqual(5);
+    expect(await db.put({ year: 2025, category: 'software' })).toEqual(6);
+
+    expect(await db.find(({ year }) => year === 2023)).toEqual({
+      id: 1,
+      year: 2023,
+      category: 'book',
+    });
+    expect(await db.find(undefined, { index: 'year', query: IDBKeyRange.only(2024) })).toEqual({
+      id: 2,
+      year: 2024,
+      category: 'book',
+    });
+    expect(await db.find(({ year }) => year === 2022)).toEqual(undefined);
+    expect(await db.findLast(({ year }) => year === 2024)).toEqual({
+      id: 5,
+      year: 2024,
+      category: 'software',
+    });
+    expect(
+      await db.findLast(undefined, { index: 'year', query: IDBKeyRange.bound(2024, 2025) }),
+    ).toEqual({
+      id: 6,
+      year: 2025,
+      category: 'software',
+    });
+    expect(await db.filter(({ category, year }) => category === 'software' && year < 2025)).toEqual(
+      [
+        {
+          id: 4,
+          year: 2023,
+          category: 'software',
+        },
+        {
+          id: 5,
+          year: 2024,
+          category: 'software',
+        },
+      ],
+    );
+    expect(
+      await db.filter(({ category }) => category === 'book', {
+        index: 'year',
+        query: IDBKeyRange.bound(2024, 2025),
+      }),
+    ).toEqual([
+      {
+        id: 2,
+        year: 2024,
+        category: 'book',
+      },
+      {
+        id: 3,
+        year: 2025,
+        category: 'book',
+      },
+    ]);
+    expect(await db.filter(({ category }) => category === 'tablet')).toEqual([]);
+  });
+});
